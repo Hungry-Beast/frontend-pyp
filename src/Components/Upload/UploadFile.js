08f4f2349eef74eb65628f3cc4aa72063350fc02
Vishal Vaibhav <vaibhavishal81@gmail.com>
2022-07-23 11:43:26 +0530

    added dropzonedialogue for upload

--- src/Components/Upload/UploadFile.js
+++ src/Components/Upload/UploadFile.js
@@ -1,11 +1,23 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 
-import { Paper, Button, Link } from "@mui/material";
+import { Paper, Button,} from "@mui/material";
+// import { Paper } from "@mui/material";
 import styled from "styled-components";
 import CloudUploadOutlinedIcon from "@mui/icons-material/CloudUploadOutlined";
+import { DropzoneDialog } from "material-ui-dropzone";
 
-import useFileUpload  from "react-use-file-upload";
-import axios from "axios";
+// import useFileUpload from "react-use-file-upload";
+
+
+
+// import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
+// import List from "@mui/material/List";
+// import ListItem from "@mui/material/ListItem";
+
+// import axios from "axios";
+
+// import FileUpload from "react-mui-fileuploader";
+// import Files from "react-files";
 
 const Box = styled.div`
   display: flex;
@@ -29,45 +41,94 @@ const IconStyle = {
   cursor: "pointer",
 };
 
-const ButtonStyle = {
-  borderRadius: "12px",
-  textTransform: "none",
-  backgroundColor: "#2b92f8",
+// const ButtonStyle = {
+//   borderRadius: "12px",
+//   textTransform: "none",
+//   backgroundColor: "#2b92f8",
+//   color: "white",
 
-  "& 	.MuiButton-text": {
-    backgroundColor: "black",
-    border: "2px solid red",
-  },
-};
+//   "& 	.MuiButton-text": {
+//     backgroundColor: "black",
+//     border: "2px solid red",
+//   },
+// };
 
-const Footer = {
-  color: "#000000",
-  marginTop: "10px",
-  textDecoration: "none",
-};
+// const DropBox = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   /* color: white; */
+//   /* background-color: black; */
+//   border-radius: 10px;
+// `;
+
+// const Footer = {
+//   color: "#000000",
+//   marginTop: "10px",
+//   textDecoration: "none",
+//   cursor: "pointer",
+// };
+
+const initialState = {
+  open: false,
+  files: []
+}
 
 const UploadFile = () => {
-  const [files, setFiles] = useState([]);
 
-  const onInputChange = (e) => {
-    setFiles(e.target.files);
+  const [state, setState] = useState(initialState);
+
+  const handleOpen = () => {
+    setState({
+      ...state,
+      open:true
+    });
+  };
+
+  const handleClose = () => {
+    setState({
+      ...state,
+      open: false
+    });
   }
+
+  const handleSave = (files) => {
+    setState({
+      ...state,
+      files: files,
+      open: false
+    });
+  };
+
+  console.log(state.files);
 
   return (
     <Box>
       <Paper elevation={5} sx={PaperStyle}>
         <CloudUploadOutlinedIcon sx={IconStyle} />
-        <Button
-          onClick={() => console.log("error")}
-          variant="contained"
-          sx={ButtonStyle}
-        >
-          Select Document
-        </Button>
+      <div>
+        
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleOpen}
+            >Add PDF</Button>
 
-        <Link sx={Footer} href="https://google.com" target="_blank">
-          Or drag and drop your file here.
-        </Link>
+            <DropzoneDialog
+              open={state.open}
+              onSave={handleSave}
+              acceptedFiles={[]}
+              showPreviews={true}
+              maxFileSize={500000000}
+              onClose={handleClose}
+              cancelButtonText={"cancel"}
+              submitButtonText={"submit"}
+              showFileNamesInPreview={true}
+              dialogTitle={"Upload File"}
+              dropzoneText={"Drag and drop here"}
+            />
+        </div>
+       
       </Paper>
     </Box>
   );
